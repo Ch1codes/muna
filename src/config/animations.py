6ef636ece71c93189080863ed41dbaa84be99673
unknown <anuj.khatakho2@gmail.com>
2025-06-24 08:48:46 +0545

    Animation generalization 1

--- src/config/animations.py
+++ src/config/animations.py
@@ -1,4 +1,6 @@
 import pygame
+from .constants import GIRL_FRAME_HEIGHT, GIRL_FRAME_WIDTH
+import os
 
 def load_frames(sheet, frame_width, frame_height, num_frames):
     frames = []
@@ -6,3 +8,13 @@ def load_frames(sheet, frame_width, frame_height, num_frames):
         frame = sheet.subsurface(pygame.Rect(i * frame_width, 0, frame_width, frame_height))
         frames.append(frame)
     return frames
+
+LILY_IDLE_IMAGE = pygame.image.load(
+    os.path.join('Assets','Girl','Idle.png')
+)
+LILY_WALK_RIGHT_IMAGE = pygame.image.load(
+    os.path.join('Assets','Girl','Walk.png')
+)
+
+idle_frames = load_frames(LILY_IDLE_IMAGE, GIRL_FRAME_WIDTH, GIRL_FRAME_HEIGHT,2)
+walkr_frames = load_frames(LILY_WALK_RIGHT_IMAGE, GIRL_FRAME_WIDTH, GIRL_FRAME_HEIGHT,6)
--- src/main.py
+++ src/main.py
@@ -3,13 +3,8 @@ import os
 
 import config.color         #import colors from color.py
 import config.constants     #import constants from constants.py
-from config.animations import load_frames
+from config.animations import idle_frames, walkr_frames
 
-LILY_IDLE_IMAGE = pygame.image.load(
-    os.path.join('Assets','Girl','Idle.png')
-)
-
-idle_frames = load_frames(LILY_IDLE_IMAGE, config.constants.GIRL_FRAME_WIDTH, config.constants.GIRL_FRAME_HEIGHT,2)
 frame_timer = 0
 frame_index = 0
 
@@ -23,12 +18,13 @@ pygame.display.set_caption("Lily's Adventure")
 def draw_window():
     global frame_timer
     global frame_index
-    frame_timer = (frame_timer + 1) % 31
-    if frame_timer==30:
-        frame_index = (frame_index + 1) % 2
+    frame_timer = (frame_timer + 1) 
+    if frame_timer==10:
+        frame_index = (frame_index + 1) % 6
+        frame_timer = 0
     print(frame_timer)
     SCREEN.fill(config.color.BLACK)
-    SCREEN.blit(idle_frames[frame_index],(600,350))
+    SCREEN.blit(walkr_frames[frame_index],(600,350))
     pygame.display.update()
 
 
